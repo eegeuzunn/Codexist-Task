cc9cacf5dd703aaa2f5d7e5f3a619ac90e4b1f46
Ege Uzun <38254705+eegeuzunn@users.noreply.github.com>
2025-04-14 14:44:36 +0300

    feat(spring): cors enabled for places endpoint

--- backend/src/main/java/com/backend/controller/PlacesController.java
+++ backend/src/main/java/com/backend/controller/PlacesController.java
@@ -8,11 +8,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/v1/places")
 @Validated
